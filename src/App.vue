73e858702052367529a9ca820b1346485078cfd2
Rayhan Mursalin <rmursalin@webmd.net>
2021-03-30 23:56:18 -0400

    Add data and API

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 	<div id="app">
 		<Header />
 		<b-container class="bv-example-row">
-      <b-row>
-        <b-col sm="6" offset="3">
-          <QuestionBox/>
-        </b-col>
-      </b-row>
-    </b-container>
+			<b-row>
+				<b-col sm="6" offset="3">
+					<QuestionBox/>
+				</b-col>
+			</b-row>
+		</b-container>
 	</div>
 </template>
 
@@ -21,6 +21,22 @@ export default {
 		Header,
 		QuestionBox
 	},
+	data() {
+		return {
+			questions: [],
+		}
+	},
+	mounted: function() {
+		fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple', {
+			method: 'get'
+		})
+			.then((response) => {
+				return response.json()
+			})
+			.then((jsonData) => {
+				this.questions = jsonData.results
+			})
+	}
 }
 </script>
 
