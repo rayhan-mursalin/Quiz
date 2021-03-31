34e24f1fed24f719561e6a37f71661c01a74ec2e
Rayhan Mursalin <rmursalin@webmd.net>
2021-03-31 00:48:33 -0400

    Shuffle answers

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -11,8 +11,7 @@
 				<b-list-group-item
 				v-for="(answer, index) in answers"
 				:key="index"
-				@click="selectAnswer(index)"
-
+				@click.prevent="selectAnswer(index)"
 				>
 				{{ answer }}
 				</b-list-group-item>
@@ -25,10 +24,17 @@
 </template>
 
 <script>
+import _ from 'lodash'
+
 export default {
 	props: {
 		currentQuestion: Object,
 		next: Function
+	},
+	data: function() {
+		return {
+			selectedIndex: null,
+		}
 	},
 	computed: {
 		answers() {
@@ -37,9 +43,23 @@ export default {
 			return answers
 		}
 	},
+	watch: {
+		currentQuestion: {
+		immediate: true,
+		handler() {
+			this.selectedIndex = null
+			this.shuffleAnswers()
+		}
+		}
+	},
 	methods: {
 		selectAnswer(index) {
-		 this.selectedIndex = index
+			this.selectedIndex = index
+		},
+		shuffleAnswers() {
+			let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+			this.shuffledAnswers = _.shuffle(answers)
+			this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
 		},
 	}
 }
@@ -55,5 +75,14 @@ export default {
 }
 .btn {
   margin: 0 5px;
+}
+.selected {
+  background-color: rgb(80, 186, 221);
+}
+.correct {
+  background-color: rgb(76, 236, 76);
+}
+.incorrect {
+  background-color: rgb(243, 44, 44);
 }
 </style>
