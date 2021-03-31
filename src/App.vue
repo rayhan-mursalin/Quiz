7d30b8caa639a33c002893edc4a8a818046833c3
Rayhan Mursalin <rmursalin@webmd.net>
2021-03-30 23:30:50 -0400

    Update header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app">
-    <Header />
-    <QuestionBox />
-  </div>
+	<div id="app">
+		<Header />
+		<QuestionBox />
+	</div>
 </template>
 
 <script>
@@ -10,21 +10,21 @@ import Header from './components/Header.vue'
 import QuestionBox from './components/QuestionBox.vue'
 
 export default {
-  name: 'App',
-  components: {
-    Header,
-    QuestionBox
-  }
+	name: 'App',
+	components: {
+		Header,
+		QuestionBox
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
